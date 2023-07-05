151b3bac340c69c05e75119a0e0c390af8dbf62d
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-07-05 22:19:17 +0400

    fix: cursor position on scroll

--- src/js/components/_cursor.js
+++ src/js/components/_cursor.js
@@ -1,16 +1,25 @@
 let cursor = null
+
+const lastMouseCoords = {
+	x: 0,
+	y: 0
+}
+
+const lastScrollCoords = {
+	x: 0,
+	y: 0
+}
+
 document.addEventListener('mousemove', e => {
 	const area = e.target.closest('[data-cursor]')
 	if (area) {
-		console.log(area.dataset.cursor)
 		const newCursor = document.querySelector(`.cursor--${area.dataset.cursor}`)
 		if (cursor && cursor !== newCursor) {
 			cursor.classList.remove('cursor--active')
 		}
 		cursor = newCursor
 		cursor.classList.add('cursor--active')
-		cursor.style.left = e.pageX + 'px'
-		cursor.style.top = e.pageY + 'px'
+		updateCursorPosition(e.pageX, e.pageY)
 	} else {
 		if (cursor) {
 			cursor.classList.remove('cursor--active')
@@ -18,3 +27,30 @@ document.addEventListener('mousemove', e => {
 		}
 	}
 })
+
+window.addEventListener('scroll', (e) => {
+	const top  = window.scrollY || document.documentElement.scrollTop
+	const left = window.scrollX || document.documentElement.scrollLeft
+	const deltaY = top - lastScrollCoords.y
+	const deltaX = left - lastScrollCoords.x
+	lastScrollCoords.y = top
+	lastScrollCoords.x = left
+	updateCursorPosition(lastMouseCoords.x+deltaX, lastMouseCoords.y + deltaY)
+
+})
+
+document.addEventListener('mouseleave', () => {
+	if (cursor) {
+		cursor.classList.remove('cursor--active')
+		cursor = null
+	}
+})
+
+const updateCursorPosition = (x, y) => {
+	if(cursor) {
+		lastMouseCoords.x = x
+		lastMouseCoords.y = y
+		cursor.style.left = x + 'px'
+		cursor.style.top = y + 'px'
+	}
+}
